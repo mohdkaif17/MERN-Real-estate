e5a5687cd5ed3a67c7647962cfc9581979c8a50d
Mohammad Kaif <kaifii0217@gmail.com>
2026-06-11 00:52:26 +0530

    Add files via upload

---
+++ api/index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoute from "./routes/user.route.js";
+import authRoute from "./routes/auth.route.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+
+app.get("/test", (req, res) => {
+  res.send("Hello World!");
+  }   
+);
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    return res.status(statusCode).json({
+      message,
+      success: false,
+      statusCode,
+    });
+  });
+ 
