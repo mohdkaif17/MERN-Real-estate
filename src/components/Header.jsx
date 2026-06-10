8e91a2d747885afd4045500f2ee5ac5256f0cbcf
Mohammad Kaif <kaifii0217@gmail.com>
2026-06-11 00:55:05 +0530

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <header className="bg-slate-200 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl">
+            <span className="text-slate-500">Supreme</span>
+            <span className="text-slate-700">Living</span>
+          </h1>
+        </Link>
+
+        <form className="bg-slate-100 p-3 rounded-lg flex items-center gap-2">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent outline-none"
+          />
+          <FaSearch className="text-slate-600" />
+        </form>
+
+        <ul className="flex gap-4">
+          <li className="hidden sm:inline text-slate-700 hover:underline">
+            <Link to="/">Home</Link>
+          </li>
+
+          <li className="hidden sm:inline text-slate-700 hover:underline">
+            <Link to="/about">About</Link>
+          </li>
+
+          <li className="text-slate-700 hover:underline">
+            <Link to="/sign-in">Sign in</Link>
+          </li>
+        </ul>
+
+      </div>
+    </header>
+  );
+}
